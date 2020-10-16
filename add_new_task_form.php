df7413245ef4daa9cf3d387febf552e50f8b5f1e
Atinder-Pal <atty.pal46@gmail.com>
2020-10-16 07:59:21 -0600

    Create basic html form to add new task

---
+++ add_new_task_form.php
@@ -0,0 +1,27 @@
+
+    <section>
+        <h2>Add new Task to your List!</h2>
+        <form action="#" method="POST">
+            <label for="new_task">
+                Add new task:            
+                <input id="new_task" type="text" value="" name="new_task" placeholder="New Task" autofocus required>
+            </label>
+            <label for="due_date">
+                Due date: 
+                <?php//Citation ?>           
+                <?php//https://stackoverflow.com/questions/43274559/how-do-i-restrict-past-dates-in-html5-input-type-date ?>           
+                <?php//Above source showed how to set min attribute to current date ?> 
+                <input id="due_date" type="date" name="due_date" min="<?php echo date("Y-m-d"); ?>" autofocus required>
+                <?php//End Citation ?>
+            </label>
+            <label for="category">
+                Task Category:            
+                <select name="species" id="species" required>
+                    <option value="">Select a category</option>
+                    <?php// echo $category_select_options; ?>
+                </select>
+            </label>
+            <input type="submit" name="addTask" value="Add to List" id="add-task-button">            
+        </form>        
+    </section>
+
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+// Show Header
+include dirname(__FILE__).'/includes/header.php';
+
+// Show Form to submit new task to List
+include dirname(__FILE__).'/add_new_task_form.php';
+
+// Show Footer
+include dirname(__FILE__).'/includes/footer.php';
