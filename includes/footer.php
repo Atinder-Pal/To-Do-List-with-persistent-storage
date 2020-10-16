a9f0f87d7813a670c61ee084e7e6623ab9808ee0
Atinder-Pal <atty.pal46@gmail.com>
2020-10-16 07:20:05 -0600

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer>    
+    <p>
+      &copy;
+      <?php echo date( 'Y' ); // Output current year (based on server computer time.) ?>      
+      Atinder Pal
+    </p>
+  </footer>
+</body>
+</html>
