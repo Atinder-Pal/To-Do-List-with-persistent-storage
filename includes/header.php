c954b5e6d3678ba6a4bac318db6aaf941c10a2ee
Atinder-Pal <atty.pal46@gmail.com>
2020-10-16 07:18:17 -0600

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To-Do App</title>
+     <!-- Style(s) -->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!-- Script(s) -->
+    <script type="text/JavaScript" src="js/scripts.js" defer></script>
+</head>
+<body>
+   <h1>Welcome to the To-Do App</h1>
+   
