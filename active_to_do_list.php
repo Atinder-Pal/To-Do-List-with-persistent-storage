e0afcb21452e69aa7bf9734cd83c188b0f13ed79
Atinder-Pal <atty.pal46@gmail.com>
2020-10-16 16:21:59 -0600

    Create HTML Table head for Active To-Do List

---
+++ active_to_do_list.php
@@ -0,0 +1,17 @@
+
+<section>
+        <h2>Active To-Do List</h2>
+        <?php// if($message) echo $message; ?>
+        <form action="#" method="POST">
+            <table>
+                <tr>
+                   <th><input type="checkbox" disabled></th> 
+                   <th>Category</th> 
+                   <th>Task</th> 
+                   <th>Due Date</th> 
+                </tr>
+                <?php //echo ?>
+            </table>
+            <input type="submit" name="add_task" value="Completed!" id="completed_button">            
+        </form>        
+    </section>
--- index.php
+++ index.php
@@ -6,5 +6,8 @@ include dirname(__FILE__).'/includes/header.php';
 // Show Form to submit new task to List
 include dirname(__FILE__).'/add_new_task_form.php';
 
+// Show Active To-Do List
+include dirname(__FILE__).'/active_to_do_list.php';
+
 // Show Footer
 include dirname(__FILE__).'/includes/footer.php';
